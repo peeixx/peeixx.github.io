10d40fc33cb1c2211acfe7b7bcb5dba4a59d51b4
peeixx <sara.peix@uvic.cat>
2022-05-25 18:38:15 +0200

    Portfoli

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const overlay = document.getElementById('contenidor-tres');
-document.querySelectorAll('.contenidor-dos .images img').forEach((element) =>{
-	const ruta = element.getAttribute('src');
-
-	element.addEventListener('click', () =>{
-		overlay.classList.add('actiu');
-	});
-});
